16e74b23113a626ae049494e9bb05dfca5b58a04
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-01-11 16:44:51 +0100

    removed spacy-code and renamed project

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# nlp - An NLP-App/Service
+# gnapp - A webapp/service for GermaNet
 
-nlpapp is a small web-application to explore [GermaNet](www.sfs.uni-tuebingen.de/GermaNet/) as well as a web-service exposing GermaNet through HTTP.
-nlpapp uses [pygermanet](https://pypi.python.org/pypi/pygermanet/) which itself requires [MongoDB](https://www.mongodb.com/de).
-nlpapp is also a small webservice to expose [spacy's nlp-functionalities](https://spacy.io/) through http
+gnapp is a small web-application to explore [GermaNet](www.sfs.uni-tuebingen.de/GermaNet/) as well as a web-service exposing GermaNet through HTTP.
+gnapp uses [pygermanet](https://pypi.python.org/pypi/pygermanet/) which itself requires [MongoDB](https://www.mongodb.com/de).
--- enrich/views.py
+++ enrich/views.py
@@ -1,4 +1,3 @@
-import spacy
 from rest_framework.decorators import api_view
 from django.shortcuts import render
 from django.views.generic.edit import FormView
@@ -6,60 +5,6 @@ from pygermanet import load_germanet
 from .forms import TokenForm, LongTextForm
 
 gn = load_germanet()
-nlp = spacy.load('de_core_news_sm')
-
-
-class TextParser(FormView):
-    template_name = 'enrich/textparser.html'
-    form_class = LongTextForm
-    success_url = '.'
-
-    def form_valid(self, form, **kwargs):
-        context = super(TextParser, self).get_context_data(**kwargs)
-        cd = form.cleaned_data
-        longtext = cd['longtext']
-        context['longtext'] = None
-        if longtext:
-            doc = nlp("{}".format(longtext))
-            context['longtext'] = longtext
-            sents = [x for x in doc.sents]
-            result = []
-            for x in sents:
-                chunk = {}
-                chunk['sent'] = x
-                chunk['tokens'] = []
-                for y in x:
-                    parts = {}
-                    parts['text'] = y.text
-                    parts['lemma'] = y.lemma_
-                    parts['pos'] = y.pos_
-                    parts['tag'] = y.tag_
-                    parts['dep'] = y.dep_
-                    parts['shape'] = y.shape_
-                    parts['is_alpha'] = y.is_alpha
-                    chunk['tokens'].append(parts)
-                result.append(chunk)
-            context['result'] = result
-        return render(self.request, self.template_name, context)
-
-
-class Lemmatize(FormView):
-    template_name = 'enrich/lemmatize.html'
-    form_class = TokenForm
-    success_url = '.'
-
-    def form_valid(self, form, **kwargs):
-        context = super(Lemmatize, self).get_context_data(**kwargs)
-        cd = form.cleaned_data
-        token = cd['token']
-        context['token'] = None
-        if token:
-            doc = nlp("{}".format(token))[0]
-            context['token'] = doc
-            context['lemma'] = doc.lemma_
-            context['pos'] = doc.pos_
-            context['tag'] = doc.tag_
-        return render(self.request, self.template_name, context)
 
 
 class TokenQuery(FormView):
